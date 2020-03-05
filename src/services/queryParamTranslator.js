89840438c23759fe62a42828708a3f2d84c68120
Kuenaimaku <Kuenaimaku@gmail.com>
2020-03-04 19:18:21 -0600

    WIP - expertise data model change

---
+++ src/services/queryParamTranslator.js
@@ -0,0 +1,8 @@
+// import expertiseDefaults from "@/expertise.json"
+export default {
+    toQueryParams(expertiseList){
+        expertiseList.forEach(function(value, key) {
+            console.log(key, value)
+        })
+    }
+}
